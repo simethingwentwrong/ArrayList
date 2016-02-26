45460e3666d46d60f64e853d3a09f04ca2bb4522
Yggdrasil <ryuk1967@gmail.com>
2016-02-26 12:54:57 +0100

    Hasta el metodo size comentarios añadidos

--- ArrayList.java
+++ ArrayList.java
@@ -38,7 +38,23 @@ public class ArrayList
      */
     public void add(int index, int elemento)
     {
-        
+        if (index <= listaLista.length && index >=0 )
+        {
+            int[] listaTonta = listaLista;
+            listaLista = new int[listaTonta.length + 1];
+            for (int count = 0; count < listaTonta.length; count ++)
+            {
+                if (index < count)
+                {
+                    listaLista[count] = listaTonta[count];
+                }
+                else 
+                {
+                    listaLista[count + 1] = listaTonta[count];
+                }
+             }
+             listaLista[index] = elemento;
+         }
     }
     
     /**
@@ -46,24 +62,43 @@ public class ArrayList
      */
     public void clear()
     {
-        
+        listaLista = new int [0];
     }
     
     /**
      * devuelve true si la colección contiene ese elemento, false en cualquier otro caso.
      */
-    public void contains(int elemento)
+    public boolean contains(int elemento)
     {
-        
+        boolean objeto = false;
+        int cont = 0;
+        while(!false && cont < listaLista.length)
+        {
+            if (listaLista[cont] == elemento)
+            {
+                objeto = true;
+            }
+            cont++;
+        }
+        return objeto;
     }
     
     /**
      *  devuelve el elemento existente en la posición indicada. 
      *  Si el valor de index no es válido, entonces devuelve -1.
      */
-    public void get(int index)
+    public int get(int index)
     {
-        
+        int objeto = 0;
+        if (index < listaLista.length && index > 0)
+        {
+            objeto = listaLista[index];
+        }
+        else
+        {
+            objeto = -1;
+        }
+        return objeto;
     }
     
     /**
@@ -72,39 +107,86 @@ public class ArrayList
      */
     public void set(int index, int element)
     {
-        
+        if (index < listaLista.length && index >= 0)
+        {
+            listaLista[index] = element;
+        }
     }
     
     /**
      *  devuelve el indice en el que aparece la primera ocurrencia del elemento especificado o -1 en caso de que la colección no contenga el elemento especificado.
      */
-    public void indexOf(int elemento)
+    public int indexOf(int elemento)
     {
-        
+        int index = 0;
+        int cont = 0;
+        if (contains(elemento))
+        {
+            while (cont < listaLista.length)
+            {
+                if (listaLista[cont] == elemento)
+                {
+                    index = cont;
+                }
+                cont++;
+            }
+        }
+        else
+        {
+            index = -1;
+        }
+        return index;
     }
     
     /**
      * devuelve true si la lista no contiene elementos.
      */
-    public void isEmpty()
+    public boolean isEmpty()
     {
-        
+        boolean empty = false;
+        if ( listaLista.length == 0)
+        {
+            empty = true;
+        }
+        return empty;
     }
     
     /**
      * elimina de la colección el elemento que ocupa la posición especificada y devuelve dicho elemento.
      * Si el valor de index no es válido, no hace nada y devuelve -1.
      */
-    public void remove(int index)
+    public int remove(int index)
     {
-        
+        int eliminado = 0;
+        if (index <= listaLista.length && index >=0)
+        {
+            int[] listaTonta = listaLista;
+            listaLista = new int[listaTonta.length - 1];
+            for (int count = 0; count < listaLista.length; count ++)
+            {
+                if (index < count)
+                {
+                    listaLista[count] = listaTonta[count];
+                }
+                else 
+                {
+                    listaLista[count] = listaTonta[count + 1];
+                }
+             }
+             eliminado = listaTonta[index];
+         }
+         else
+         {
+             eliminado = -1;
+            }
+        return eliminado;
     }
     
     /**
      * devuelve el número de elementos de la colección.
      */
-    public void size()
+    public int size()
     {
-        
+        return listaLista.length;
     }
 }
